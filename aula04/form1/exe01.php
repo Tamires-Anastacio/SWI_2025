ee86c724fcfaba25e97937751c32d62fd73da161
Tamires <159663594+Tamires-Anastacio@users.noreply.github.com>
2025-03-06 09:10:53 -0300

    Add files via upload

---
+++ aula04/form1/exe01.php
@@ -0,0 +1,6 @@
+<?php
+   $nome = $GET ['nome'];
+   $nota1 = $GET ['nota1'];
+   $nota2 = $GET ['nota2'];
+   $nota3 = $GET ['nota3'];
+?>
---
+++ aula04/form2/exe02.php
@@ -0,0 +1,10 @@
+<?php
+   $nome = $GET ['nome'];
+   $nota1 = $GET ['n1'];
+   $nota2 = $GET ['n2'];
+   $nota3 = $GET ['n3'];
+
+   if ($n1>$n2 && $n1>$n3 & $n2>$n3); {
+    elseif($n2>$n1 && $n2>$n3 & $n1>$n3);
+   }
+?>
