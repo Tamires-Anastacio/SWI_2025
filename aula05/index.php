88b0006a66469ed5cb9d17cba2b815c439cc17aa
Tamires <159663594+Tamires-Anastacio@users.noreply.github.com>
2025-03-17 08:26:54 -0300

    Add files via upload

---
+++ aula05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class = "container">
+        <form action="processa.php" method = "POST">
+            <!--email input-->
+            <div data-mdb-input-init class="form outline mb-4">
+                <input type="email"  id="form2Example1" class="form-control" name="email"/>
+                <label class="form-label" for="form1Example1"></label>
+        </div>
+    </div>
+    
+</body>
+</html>
